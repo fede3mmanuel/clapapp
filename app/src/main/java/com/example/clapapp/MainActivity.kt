b0caf844a20db9c7687c27f369dbc23e0926b11b
Emmanuel Sanchez <fede@fede.website>
2023-01-24 11:29:29 -0600

    the buttons to execute the audio works correctly

--- app/src/main/java/com/example/clapapp/MainActivity.kt
+++ app/src/main/java/com/example/clapapp/MainActivity.kt
@@ -4,18 +4,34 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mediaPlayer: MediaPlayer
+    private var mediaPlayer: MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.applauding)
+        val playButton = findViewById<FloatingActionButton>(R.id.fabPlay)
+        playButton.setOnClickListener{
+            if (mediaPlayer == null){
+                mediaPlayer = MediaPlayer.create(this, R.raw.applauding)
+            }
 
-        //val button = findViewById<Button>(R.id.btnClap)
-        //button.setOnClickListener {
-        //    mediaPlayer.start()
-        //}
+            mediaPlayer?.start()
+        }
+
+        val pauseButton = findViewById<FloatingActionButton>(R.id.fabPause)
+        pauseButton.setOnClickListener {
+            mediaPlayer?.pause()
+        }
+
+        val stopButton = findViewById<FloatingActionButton>(R.id.fabStop)
+        stopButton.setOnClickListener {
+            mediaPlayer?.stop()
+            mediaPlayer?.reset()
+            mediaPlayer?.release()
+            mediaPlayer = null
+        }
     }
 }
