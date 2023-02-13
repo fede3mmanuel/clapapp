58c6c08f00099d71488f30f76ebc53d02a9939ba
Emmanuel Sanchez <fede@fede.website>
2023-02-13 13:36:38 -0600

    seekbar is working

--- app/src/main/java/com/example/clapapp/MainActivity.kt
+++ app/src/main/java/com/example/clapapp/MainActivity.kt
@@ -3,19 +3,27 @@ package com.example.clapapp
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
+import android.widget.SeekBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var seekBar: SeekBar
     private var mediaPlayer: MediaPlayer? = null
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        seekBar = findViewById(R.id.sbClapping)
+        handler = Handler(Looper.getMainLooper())
         val playButton = findViewById<FloatingActionButton>(R.id.fabPlay)
         playButton.setOnClickListener{
             if (mediaPlayer == null){
                 mediaPlayer = MediaPlayer.create(this, R.raw.applauding)
+                initializeSeekBar()
             }
 
             mediaPlayer?.start()
@@ -32,6 +40,34 @@ class MainActivity : AppCompatActivity() {
             mediaPlayer?.reset()
             mediaPlayer?.release()
             mediaPlayer = null
+            handler.removeCallbacks(runnable)
+            seekBar.progress = 0
         }
+    }
+
+    private fun initializeSeekBar(){
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser) mediaPlayer?.seekTo(progress)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
+        seekBar.max = mediaPlayer!!.duration
+        runnable = Runnable {
+            seekBar.progress = mediaPlayer!!.currentPosition
+
+            handler.postDelayed(runnable, 1000)
+        }
+
+        handler.postDelayed(runnable, 1000)
     }
 }
